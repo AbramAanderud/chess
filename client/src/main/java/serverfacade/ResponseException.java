4d9b87d372a7b2196023e83c6721e310e0e762d7
Abram Aanderud <abramashaanderud@gmail.com>
2024-11-12 14:33:04 -0700

    Making server facade stubs with pet shop dont know if this will work

---
+++ client/src/main/java/serverfacade/ResponseException.java
@@ -0,0 +1,14 @@
+package serverfacade;
+
+public class ResponseException extends Throwable {
+    final private int statusCode;
+
+    public ResponseException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int StatusCode() {
+        return statusCode;
+    }
+}

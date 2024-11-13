2004cd2c629eff8e17681185c7eee8882a4e137c
Abram Aanderud <abramashaanderud@gmail.com>
2024-11-12 17:30:07 -0700

    finished login need to test

--- client/src/main/java/chessClient/ChessClient.java
+++ client/src/main/java/chessClient/ChessClient.java
@@ -1,20 +1,24 @@
 package chessClient;
 
 import com.sun.nio.sctp.NotificationHandler;
+import dataaccess.DataAccessException;
 import repl.State;
+import requests.LoginRequest;
+import requests.RegisterRequest;
+import result.LoginResult;
 import serverfacade.ResponseException;
 import serverfacade.ServerFacade;
 
 import java.util.Arrays;
 
+import static repl.State.*;
+
 public class ChessClient {
     private final ServerFacade server;
-    private final String serverUrl;
-    private State state = State.SIGNEDOUT;
+    private State state = SIGNEDOUT;
 
-    public ChessClient(String serverUrl) {
-        server = new ServerFacade(serverUrl);
-        this.serverUrl = serverUrl;
+    public ChessClient(String serverURL) {
+        server = new ServerFacade(serverURL);
     }
 
     public String evalUnsignedIn(String input) {
@@ -34,18 +38,40 @@ public class ChessClient {
     }
 
     public String login(String... params) throws ResponseException {
-        if (params.length >= 1) {
-            state = State.SIGNEDIN;
+        if (params.length >= 2) {
+            String username = params[0];
+            String password = params[1];
 
-            server.login();
-            return String.format("You signed in as %s.", params[0]);
+            LoginRequest loginRequest = new LoginRequest(username, password);
+
+            try {
+                LoginResult loginResult = server.login(loginRequest);
+
+                if (loginResult.authToken() != null) {
+                    state = SIGNEDIN;
+                    return String.format("You signed in as %s.", username);
+                } else {
+                    return "Login failed: " + (loginResult.message());
+                }
+            } catch (DataAccessException e) {
+                return "Error signing in";
+            }
         }
         throw new ResponseException(400, "Expected: <yourname>");
     }
 
     public String register(String... params) throws ResponseException {
-        if (params.length >= 1) {
-            state = State.SIGNEDIN;
+        if (params.length >= 3) {
+            String username = params[0];
+            String password = params[1];
+            String email = params[2];
+
+            RegisterRequest registerRequest = new RegisterRequest(username, password, email);
+
+            try {
+                
+            }
+            state = SIGNEDIN;
 
             return String.format("You signed in as %s.", params[0]);
         }
